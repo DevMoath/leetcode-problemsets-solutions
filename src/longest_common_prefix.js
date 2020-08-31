fa73a16d74d69230bfbce00c6d6afbd176e7936c
Moath <moath.alhajrii@gmail.com>
2020-08-31 11:30:40 +0300

    add solution of longest_common_prefix problem

---
+++ src/longest_common_prefix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = (strs) => {
+    let prefix = "", length = Math.max(...(strs.map(s => s.length)));
+    
+    for (let i = 0; i < length; i++) {
+        let temp = [];
+        
+        for (let j = 0; j < strs.length; j++) {
+            temp.push(strs[j][i]);
+        }
+        
+        temp = new Set(temp);
+        
+        if (temp.size !== 1) {
+            break;
+        }
+        
+        prefix += temp.values().next().value;
+    }
+    
+    return prefix;
+};
+
+// Test cases
+console.log(longestCommonPrefix(["flower", "flow", "flight"]) === "fl");
+console.log(longestCommonPrefix(["dog", "racecar", "car"]) === "");
