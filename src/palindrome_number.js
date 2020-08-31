5b94aed54d5e5bdef2ce16e3e85a094143f31091
Moath <moath.alhajrii@gmail.com>
2020-08-31 09:40:36 +0300

    add solution of palindrome_number problem

---
+++ src/palindrome_number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = (x) => {
+    if (x < 0) {
+        return false;
+    }
+    
+    return x == String(x).split("").reverse().join("");
+};
+
+// Test cases
+console.log(isPalindrome(121) === true);
+console.log(isPalindrome(-121) === false);
+console.log(isPalindrome(10) === false);
+console.log(isPalindrome(1221) === true);
