31cf02724c5e974cb2e8698644f74598d8a1fc19
Moath <moath.alhajrii@gmail.com>
2020-09-07 13:09:27 +0300

    add isEquals to add_two_numbers problem

--- src/add_two_numbers.js
+++ src/add_two_numbers.js
@@ -28,8 +28,33 @@ const addTwoNumbers = (l1, l2, carry = null) => {
     return node;
 };
 
-// Test cases
-let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {boolean}
+ */
+const isEquals = (l1, l2) => {
+    let pointerL1 = l1;
+    let pointerL2 = l2;
+    
+    while (pointerL1 || pointerL2) {
+        let value1 = pointerL1 ? pointerL1.val : 0;
+        let value2 = pointerL2 ? pointerL2.val : 0;
+        
+        if (value1 !== value2) {
+            return false;
+        }
+        
+        pointerL1 = pointerL1.next;
+        pointerL2 = pointerL2.next;
+    }
+    
+    return true;
+};
 
-console.log(addTwoNumbers(l1, l2));
+// Test cases
+let l1       = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2       = new ListNode(5, new ListNode(6, new ListNode(4)));
+let expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+
+console.log(isEquals(addTwoNumbers(l1, l2), expected) === true);
