b599df40458a5e0998242a37323c422770467b02
Moath <moath.alhajrii@gmail.com>
2020-09-07 11:22:13 +0300

    add solution of valid-parentheses problem

---
+++ src/valid-parentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+    const mappings = new Map([
+        [")", "("],
+        ["}", "{"],
+        ["]", "["],
+    ]);
+    
+    let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        
+        if (mappings.has(c)) {
+            let topElement = stack.length === 0 ? "#" : stack.pop();
+            
+            if (topElement !== mappings.get(c)) {
+                return false;
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+    
+    return stack.length === 0;
+};
+
+// Test cases
+console.log(isValid("()") === true);
+console.log(isValid("()[]{}") === true);
+console.log(isValid("(]") === false);
+console.log(isValid("([)]") === false);
+console.log(isValid("{[]}") === true);
